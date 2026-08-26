b8f8af089d9f591ce97f71278ddba0da6d1d5127
pownz123 <ponponbajao@example.com>
2026-08-26 10:30:45 +0800

    Your commit message

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
 
-Route ::apiResource('students',  StudentController::class);
 
-Route ::middlewer(['auth'])->group(function(){
-    Route::get('dashboard', function() {
-         return Inertia ::render('dashboard');
-
-    })->name('dashboard');
-   
-});
 require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+
